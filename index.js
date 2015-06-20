a8b680a0356ef15711391153c3e822c10099bd8b
Mike Nikles <mike@vanniksoftware.com>
2015-06-20 12:10:22 -0700

    Added more test cases; simplified the default use case where no processing instructions are required

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var html2react = require('./lib/html2react');
 var processingInstructions = require('./lib/processing-instructions');
 var isValidNodeDefinitions = require('./lib/is-valid-node-definitions');
+var processNodeDefinitions = require('./lib/process-node-definitions');
 
 module.exports = {
     Html2React: html2react,
+    ProcessingInstructions: processingInstructions,
     IsValidNodeDefinitions: isValidNodeDefinitions,
-    ProcessingInstructions: processingInstructions
+    ProcessNodeDefinitions: processNodeDefinitions,
 };
 
--- test/html2react-tests.js
+++ test/html2react-tests.js
@@ -4,19 +4,16 @@ var assert = require("assert");
 var React = require('react');
 
 var Html2React = require('../index').Html2React;
-var ProcessingInstructions = require('../index').ProcessingInstructions;
-var IsValidNodeDefinitions = require('../index').IsValidNodeDefinitions;
+var ProcessNodeDefinitions = require('../index').ProcessNodeDefinitions;
 
 describe('Html2React', function() {
     var html2react = new Html2React(React);
-    var processingInstructions = new ProcessingInstructions(React);
 
     describe('parse valid HTML', function() {
         it('should return a valid HTML string', function() {
-            var htmlInput = '<div>Does this work?</div>';
+            var htmlInput = '<p>Does this work?</p>';
 
-            var reactComponent = html2react.parse(htmlInput,
-                IsValidNodeDefinitions.isAlwaysValidNode, processingInstructions.defaultProcessingInstructions);
+            var reactComponent = html2react.parse(htmlInput);
             var reactHtml = React.renderToStaticMarkup(reactComponent);
 
             assert.equal(reactHtml, htmlInput);
@@ -25,8 +22,7 @@ describe('Html2React', function() {
         it('should return a valid HTML string with nested elements', function() {
             var htmlInput = '<div><h1>Heading</h1></div>';
 
-            var reactComponent = html2react.parse(htmlInput,
-                IsValidNodeDefinitions.isAlwaysValidNode, processingInstructions.defaultProcessingInstructions);
+            var reactComponent = html2react.parse(htmlInput);
             var reactHtml = React.renderToStaticMarkup(reactComponent);
 
             assert.equal(reactHtml, htmlInput);
@@ -35,11 +31,108 @@ describe('Html2React', function() {
         it('should return a valid HTML string with inline styles', function() {
             var htmlInput = '<div style="background-color: red;color: white;"></div>';
 
-            var reactComponent = html2react.parse(htmlInput,
-                IsValidNodeDefinitions.isAlwaysValidNode, processingInstructions.defaultProcessingInstructionsWithInlineStyle);
+            var reactComponent = html2react.parse(htmlInput);
             var reactHtml = React.renderToStaticMarkup(reactComponent);
 
             assert.equal(reactHtml, htmlInput);
+        });
+
+        it('should return a valid HTML string with data attributes', function() {
+            var htmlInput = '<div data-test-attribute="data attribute value"></div>';
+
+            var reactComponent = html2react.parse(htmlInput);
+            var reactHtml = React.renderToStaticMarkup(reactComponent);
+
+            assert.equal(reactHtml, htmlInput);
+        });
+
+        it('should return a valid HTML string with aria attributes', function() {
+            var htmlInput = '<div aria-labelledby="label1"></div>';
+
+            var reactComponent = html2react.parse(htmlInput);
+            var reactHtml = React.renderToStaticMarkup(reactComponent);
+
+            assert.equal(reactHtml, htmlInput);
+        });
+
+        it('should return a valid HTML string with a class attribute', function() {
+            var htmlInput = '<div class="class-one"></div>';
+
+            var reactComponent = html2react.parse(htmlInput);
+            var reactHtml = React.renderToStaticMarkup(reactComponent);
+
+            assert.equal(reactHtml, htmlInput);
+        });
+    });
+
+    describe('parse invalid HTML', function() {
+        it('should throw an error when trying parsing multiple root elements', function() {
+            var htmlInput = '<div></div><div></div>';
+
+            assert.throws(function() {
+                html2react.parse(htmlInput);
+            }, Error);
+        });
+
+        it('should throw an error with a specific message when parsing multiple root elements', function() {
+            var htmlInput = '<div></div><div></div><div></div>';
+
+            assert.throws(function() {
+                html2react.parse(htmlInput);
+            }, /contains 3 root elements/);
+        });
+
+        it('should fix missing closing tags', function() {
+            var htmlInput = '<div><p></div>';
+            var htmlExpected = '<div><p></p></div>';
+
+            var reactComponent = html2react.parse(htmlInput);
+            var reactHtml = React.renderToStaticMarkup(reactComponent);
+
+            assert.equal(reactHtml, htmlExpected);
+        });
+    });
+});
+
+describe('Html2React with custom processing instructions', function() {
+    var html2react = new Html2React(React);
+    var processNodeDefinitions = new ProcessNodeDefinitions(React);
+
+    describe('parse valid HTML', function() {
+        it('should return nothing with only a single <p> element', function() {
+            var htmlInput = '<p>Does this work?</p>';
+            var isValidNode = function() {
+                return true;
+            };
+            var processingInstructions = [{
+                shouldProcessNode: function(node) {
+                    return node.name && node.name !== 'p';
+                },
+                processNode: processNodeDefinitions.processDefaultNode
+            }];
+            var reactComponent = html2react.parseWithInstructions(htmlInput, isValidNode, processingInstructions);
+
+            // With only 1 <p> element, nothing is rendered
+            assert.equal(reactComponent, false);
+        });
+
+        it('should return a single <h1> element within a div of <h1> and <p> as siblings', function() {
+            var htmlInput = '<div><h1>Title</h1><p>Paragraph</p></div>';
+            var htmlExpected = '<div><h1>Title</h1></div>';
+
+            var isValidNode = function() {
+                return true;
+            };
+
+            var processingInstructions = [{
+                shouldProcessNode: function(node) {
+                    return node.type === 'text' || node.name !== 'p';
+                },
+                processNode: processNodeDefinitions.processDefaultNode
+            }];
+            var reactComponent = html2react.parseWithInstructions(htmlInput, isValidNode, processingInstructions);
+            var reactHtml = React.renderToStaticMarkup(reactComponent);
+            assert.equal(reactHtml, htmlExpected);
         });
     });
 });
