baffd3edfc0a75b15a52cf4534c0ab170f97aafb
Arve Knudsen <arve.knudsen@gmail.com>
2021-05-02 10:39:55 +0200

    Make sure event handlers are functions
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- test/html-to-react-tests.js
+++ test/html-to-react-tests.js
@@ -270,20 +270,12 @@ describe('Html2React', () => {
     });
 
     it('should handle onclick attributes', function () {
-      const htmlInput = '<button onclick="alert(\'hello!\')">Hello</button>';
+      const htmlInput = `<button onclick="alert('hello!')">Hello</button>`;
 
       const reactElem = parser.parse(htmlInput);
 
-      assert.strictEqual(reactElem.props.onClick, 'alert(\'hello!\')');
-    });
-
-    it('should return a valid HTML string with onclick attributes', function () {
-      const htmlInput = '<button onclick="alert(\'hello!\')">Hello</button>';
-
-      const reactElem = parser.parse(htmlInput);
-      const reactHtml = ReactDOMServer.renderToStaticMarkup(reactElem);
-
-      assert.strictEqual(htmlInput, reactHtml);
+      assert.strictEqual(typeof reactElem.props.onClick, 'function');
+      assert.strictEqual(String(reactElem.props.onClick), String(Function(`alert('hello!')`)));
     });
 
     it('should handle inputs with empty value attribute', function () {
